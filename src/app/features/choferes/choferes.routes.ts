8a307d6c203e92234bd58df8ed27038e317446db
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-16 16:22:39 -0300

    routing para gestión de choferes

--- src/app/features/choferes/choferes.routes.ts
+++ src/app/features/choferes/choferes.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ChoferesDashboardComponent } from './pages/dashboard/choferes-dashboard.component';
+import { ChoferDetailsComponent } from './pages/details/chofer-details.component';
 
 const routes: Routes = [
-    { path: '', component: ChoferesDashboardComponent }
+    { path: '', component: ChoferesDashboardComponent },
+    { path: ':dni', component: ChoferDetailsComponent }
 ];
 
 @NgModule({
